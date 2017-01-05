8e66576813dc4e0d42b3ffcfa9aa8e9a6f573698
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-05 16:46:33 +0100

    Add basic routing and make function.php usable
    add also basic header

--- lib/functions.php
+++ lib/functions.php
@@ -5,15 +5,29 @@ vous ajouterez ici les fonctions qui vous sont utiles dans le site,
 je vous ai créé la première qui est pour le moment incomplète et qui devra contenir
 la logique pour choisir la page à charger
 */
-
+$authorizedPages = array('contact', 'bio');
 function getContent(){
 	if(!isset($_GET['page'])){
 		include __DIR__.'/../pages/home.php';
-	} else {
-		// le reste du code
+	}else{
+		$page =  __DIR__.'/../pages/'.$_GET['page'].'.php';
+		if(in_array($_GET['p'], $authorizedPages) && file_exists($page)){
+			return $page;
+		}
+		else{
+			return '../parts/404.php';
+		}
 	}
 }
 
 function getPart($name){
 	include __DIR__ . '/../parts/'. $name . '.php';
+}
+
+function title(){
+	if(isset($_GET['page'])	) {
+		echo ucfirst(strtolower($_GET['page']));
+	}else{
+		echo "Home";
+	}
 }
--- parts/header.php
+++ parts/header.php
@@ -1,1 +1,16 @@
-put some header here
+
+<DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>
+			<?php 
+			title();?>
+		</title>
+		<link rel="stylesheet" href="../public/style.css">
+	</head>
+	<body>
+		<div>
+			<h2>Super site !</h2>
+		</div>
+		<?php getPart('menu'); ?>
