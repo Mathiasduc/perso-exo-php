8772d67311b6685ca086ba118ded9d28ffd1f081
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-09 09:38:07 +0100

    Add exercise 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loops</title>
+</head>
+<body>
+	<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	?>
+	<div>BOnjour</div>	
+	<?php
+	include __DIR__ . '/loop.php';
+	?>
+	
+</body>
+</html>
---
+++ loop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loops</title>
+</head>
+<body>
+	<?php 
+	$a = 0;
+	for ($a=0; $a <= 10; $a++) { 
+		echo "$a \n";
+	}
+
+	 ?>
+</body>
+</html>
